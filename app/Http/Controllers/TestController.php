19126c02ce4e3e5bd7b3fb5f96eea2b80dc5179a
william Gay <wgay1127@gmail.com>
2026-03-19 01:13:09 -0400

    added api call to jsonPlaceholder

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,10 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Http;
+use App\Http\Requests;
 class TestController extends Controller
 {
     public function index(){
+{
+    try {
+        $response = Http::get("https://jsonplaceholder.typicode.com/posts");
+        $data = $response->json(); // Automatically decodes JSON response
+        dd($data);
+
+        return response()->json($data);
+    } catch (\Exception $e) {
+        return response()->json(['error' => 'Failed to fetch data'], 500);
+    }
+}   
+       // $test = curl -X GET "https://jsonplaceholder.typicode.com/posts";
         dd("here not there");
     }
 }
