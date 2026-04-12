8bd68860c51620a4bc886b663e5dbdf19ed7b62c
William Gay <wgay1127@gmail.com>
2026-04-12 00:46:32 -0400

    Updated layout and home page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,1 @@
 import './bootstrap';
-alert('Hello, world!');
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,7 +8,7 @@
 
             @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-<body class = "bg-slate-300 text-slate-800 min-h-screen relativetext-sm sm:text-base">
+<body class = "bg-gray-600 text-slate-800 min-h-screen relativetext-sm sm:text-base">
          <div class="container-fluid">
             <nav class="relative bg-gray-800/50 after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10">
   <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
